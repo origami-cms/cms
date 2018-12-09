b07dadb61c69b9d0b17fae1aceb5600646157a03
Tristan Matthias <hello@tristanmatthias.com>
2018-12-09 19:50:17 +1100

    fix(core-lib): fixed bug with type definition files in colors

--- packages/core-lib/src/colors.ts
+++ packages/core-lib/src/colors.ts
@@ -1,40 +1,40 @@
-import chalk from 'chalk';
+import * as chalk from 'chalk';
 
 export namespace colors {
-    /**
-     * Purple chalk color
-     */
-    export const purple    = chalk.hex('#693a91');
-    /**
-     * Orange chalk color
-     */
-    export const orange    = chalk.hex('#ff791f');
-    /**
-     * Blue chalk color
-     */
-    export const blue      = chalk.hex('#23a8e6');
-    /**
-     * Red chalk color
-     */
-    export const red       = chalk.hex('#ff5554');
-    /**
-     * Red chalk color
-     */
-    export const redStrong = chalk.hex('#ff2222');
-    /**
-     * Green chalk color
-     */
-    export const green     = chalk.hex('#28c787');
-    /**
-     * Yellow chalk color
-     */
-    export const yellow    = chalk.hex('#f8c555');
-    /**
-     * Grey chalk color
-     */
-    export const grey      = chalk.hex('#454347');
-    /**
-     * White chalk color
-     */
-    export const white     = chalk.white;
+  /**
+   * Purple chalk color
+   */
+  export const purple = chalk.default.hex('#693a91');
+  /**
+   * Orange chalk color
+   */
+  export const orange = chalk.default.hex('#ff791f');
+  /**
+   * Blue chalk color
+   */
+  export const blue = chalk.default.hex('#23a8e6');
+  /**
+   * Red chalk color
+   */
+  export const red = chalk.default.hex('#ff5554');
+  /**
+   * Red chalk color
+   */
+  export const redStrong = chalk.default.hex('#ff2222');
+  /**
+   * Green chalk color
+   */
+  export const green = chalk.default.hex('#28c787');
+  /**
+   * Yellow chalk color
+   */
+  export const yellow = chalk.default.hex('#f8c555');
+  /**
+   * Grey chalk color
+   */
+  export const grey = chalk.default.hex('#454347');
+  /**
+   * White chalk color
+   */
+  export const white = chalk.default.white;
 }
