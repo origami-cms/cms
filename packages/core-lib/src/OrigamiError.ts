2022b92ae7b4e239cdd588aa64aada4a2a85c420
Tristan Matthias <hello@tristanmatthias.com>
2018-12-11 22:29:44 +1100

    doc(core-lib): documented OrigamiError

--- packages/core-lib/src/OrigamiError.ts
+++ packages/core-lib/src/OrigamiError.ts
@@ -2,6 +2,13 @@ export class OrigamiError extends Error {
   public code: string;
   public showTrace: boolean = true;
 
+  /**
+   * A custom error in Origami
+   * @param namespace Global namespace of the error
+   * @param name Name of the error
+   * @param message Error message
+   * @param code Response code to send
+   */
   constructor(
     public namespace: string = 'Origami',
     public name: string,
