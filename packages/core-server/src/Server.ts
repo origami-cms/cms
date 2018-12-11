3a8153dd0fcfb15bb26601b5befb6a37ea439150
Tristan Matthias <hello@tristanmatthias.com>
2018-12-11 21:02:17 +1100

    feat(core-server): changed server.static to use post-render position

--- packages/core-server/src/Server.ts
+++ packages/core-server/src/Server.ts
@@ -1,4 +1,15 @@
-import { colors, error, info, log, Origami, requireLib, Route, RouterListItem, success, wrapMiddleware } from '@origami/core-lib';
+import {
+  colors,
+  error,
+  info,
+  log,
+  Origami,
+  requireLib,
+  Route,
+  RouterListItem,
+  success,
+  wrapMiddleware
+} from '@origami/core-lib';
 import bodyParser from 'body-parser';
 // @ts-ignore
 import corser from 'corser';
@@ -23,9 +34,7 @@ import { format as mwFormat } from './middleware/format/format';
 import { runScripts } from './scripts/runScripts';
 const statAsync = promisify(stat);
 
-type positionRouters = {
-  [K in Origami.Server.Position]: Router
-};
+type positionRouters = { [K in Origami.Server.Position]: Router };
 
 interface NamedMiddleware {
   [name: string]: Origami.Server.RequestHandler;
@@ -36,7 +45,6 @@ const DEFAULT_STATIC = 'public';
 
 export { lib } from './lib';
 export * from './lib/App';
-
 
 export class Server {
   public app: Application;
@@ -50,11 +58,7 @@ export class Server {
   private _server?: Http2Server;
   private _namedMiddleware: NamedMiddleware = {};
 
-
-  constructor(
-    options?: Origami.ConfigServer,
-    store?: any
-  ) {
+  constructor(options?: Origami.ConfigServer, store?: any) {
     this.app = express();
     this.store = store;
 
@@ -63,13 +67,15 @@ export class Server {
       ...{
         port: parseInt(process.env.PORT || DEFAULT_PORT),
         ln: 'enUS',
-        secret: 'secret',
-      }, ...options
+        secret: 'secret'
+      },
+      ...options
     };
 
     // Special override for PORT in the environment variable
-    if (process.env.PORT) { this._options.port = parseInt(process.env.PORT); }
-
+    if (process.env.PORT) {
+      this._options.port = parseInt(process.env.PORT);
+    }
 
     // Different positions to run route at
     this._positionRouters = {
@@ -86,7 +92,6 @@ export class Server {
       'pre-send': express.Router()
     };
 
-
     // Validate the options
     // try {
     //     requireKeys(['secret'], this._options);
@@ -97,12 +102,10 @@ export class Server {
     // tslint:disable-next-line no-floating-promises
     this._setup();
 
-
     this.app.set('ln', this._options.ln);
     this.app.set('secret', this._options.secret);
     this.app.set('apps', this.apps);
   }
-
 
   // Runs the app
   public async serve() {
@@ -125,32 +128,34 @@ export class Server {
     await runScripts(this);
   }
 
-
   public stop() {
-    if (this._server) { this._server.close(); }
+    if (this._server) {
+      this._server.close();
+    }
   }
-
 
   // Add the Router's routes in each position to the middleware
   public useRouter(router: Route, requireNamedMiddleware: boolean = true) {
     Object.entries(this._positionRouters).forEach(([p, pr]) => {
-
-      router.routers[p as Origami.Server.Position]
-        .forEach(({ handlers, method }: RouterListItem) => {
-
+      router.routers[p as Origami.Server.Position].forEach(
+        ({ handlers, method }: RouterListItem) => {
           // Convert all the named handlers (EG: router.use('auth')) into request handlers
-          const mappedNamedHandlers = handlers.map((h) => {
-            if (typeof h === 'function') { return h; }
-            if (!this._namedMiddleware[h]) {
-              if (requireNamedMiddleware) {
-                throw new err.ErrorServerUnknownNamedMiddleware(h);
-              } else {
-                error(`Could not load named middleware ${h}`);
-                return false;
+          const mappedNamedHandlers = handlers
+            .map((h) => {
+              if (typeof h === 'function') {
+                return h;
               }
-            }
-            return this._namedMiddleware[h];
-          }).filter((h) => h) as Origami.Server.RequestHandler[];
+              if (!this._namedMiddleware[h]) {
+                if (requireNamedMiddleware) {
+                  throw new err.ErrorServerUnknownNamedMiddleware(h);
+                } else {
+                  error(`Could not load named middleware ${h}`);
+                  return false;
+                }
+              }
+              return this._namedMiddleware[h];
+            })
+            .filter((h) => h) as Origami.Server.RequestHandler[];
 
           // Wrap all handlers to catch any async errors
           const errorWrapped = mappedNamedHandlers.map(wrapMiddleware);
@@ -164,7 +169,9 @@ export class Server {
                 colors.yellow(p),
                 m.toUpperCase(),
                 colors.yellow(_path.toString()),
-                res.locals.content.hasContent ? colors.green('has content') : colors.red('no content')
+                res.locals.content.hasContent
+                  ? colors.green('has content')
+                  : colors.red('no content')
               );
               next();
             });
@@ -177,12 +184,13 @@ export class Server {
             'Server',
             `Successfully connected ${_path} route: `,
             colors.yellow(method.toUpperCase()),
-            colors.yellow(_path.toString()));
-        });
+            colors.yellow(_path.toString())
+          );
+        }
+      );
     });
     router.nested.forEach((_r) => this.useRouter(_r, requireNamedMiddleware));
   }
-
 
   public async plugin(
     name: string,
@@ -194,8 +202,10 @@ export class Server {
       let plugin;
 
       try {
-        plugin = await requireLib(name, p, ['@origami/plugin-', 'origami-plugin-']);
-
+        plugin = await requireLib(name, p, [
+          '@origami/plugin-',
+          'origami-plugin-'
+        ]);
       } catch (e) {
         throw new err.ErrorServerUnknownPlugin(name);
       }
@@ -204,7 +214,9 @@ export class Server {
         // Allow for importing es6 modules with require (usually experted from typescript)
         if (plugin && typeof plugin.default === 'function') {
           plugin = plugin.default;
-        } else { throw new err.ErrorServerPluginNotFunction(name); }
+        } else {
+          throw new err.ErrorServerPluginNotFunction(name);
+        }
       }
 
       await plugin(this, settings);
@@ -217,20 +229,26 @@ export class Server {
     return false;
   }
 
-
   public async application(name: string, settings: boolean | object = true) {
     if (Boolean(settings)) {
       const app = new App.App(name, this, settings);
       await app.setup();
 
-      success('Server', 'Successfully initialized application', colors.yellow(name));
+      success(
+        'Server',
+        'Successfully initialized application',
+        colors.yellow(name)
+      );
       return app;
-    } else { return Promise.resolve(false); }
+    } else {
+      return Promise.resolve(false);
+    }
   }
 
-
   public resource(name: string, options: ResourceOptions) {
-    if (!this.store) { throw new err.ErrorServerStoreNotConfigured(); }
+    if (!this.store) {
+      throw new err.ErrorServerStoreNotConfigured();
+    }
     const r = new Resource(name, this.store, options);
     this.useRouter(r.router);
     this.resources[name] = r;
@@ -238,75 +256,89 @@ export class Server {
     return r;
   }
 
-
   public namedMiddleware(name: string, handler: Origami.Server.RequestHandler) {
-    if (this._namedMiddleware[name]) { throw new err.ErrorServerExistingNamedMiddleware(name); }
+    if (this._namedMiddleware[name]) {
+      throw new err.ErrorServerExistingNamedMiddleware(name);
+    }
     this._namedMiddleware[name] = handler;
   }
-
 
   public getNamedMiddleware(
     name?: string | RegExp
   ): Origami.Server.RequestHandler | NamedMiddleware | false {
-
-    if (!name) { return { ...this._namedMiddleware }; }
+    if (!name) {
+      return { ...this._namedMiddleware };
+    }
 
     // Filter out the middleware by the regex name
     if (name instanceof RegExp) {
       return Object.entries(this._namedMiddleware)
         .filter(([key]) => name.test(key))
-        .reduce((_mw, [key, obj]) => {
-          _mw[key] = obj;
-          return _mw;
-        }, {} as NamedMiddleware);
+        .reduce(
+          (_mw, [key, obj]) => {
+            _mw[key] = obj;
+            return _mw;
+          },
+          {} as NamedMiddleware
+        );
     }
 
     const mw = this._namedMiddleware[name];
     return mw || false;
   }
 
-
   // Return a list of all middleware in the position routers that match the path
   // (exposes Express Layers and their 'handle')
   public getMiddlewareForPath(p: string): Function[] {
-    const mw = Object.values(this._positionRouters).reduce((_mw, r) => {
-      _mw.push(
-        ...r.stack
-          .filter((layer) => layer.match(p))
-          .map((layer) => {
-            if (layer.route) { return layer.route.stack; } else { return layer.handler; }
-          }) as Function[]
-      );
-      return _mw;
-    }, [] as Function[]);
+    const mw = Object.values(this._positionRouters).reduce(
+      (_mw, r) => {
+        _mw.push(
+          ...(r.stack
+            .filter((layer) => layer.match(p))
+            .map((layer) => {
+              if (layer.route) {
+                return layer.route.stack;
+              } else {
+                return layer.handler;
+              }
+            }) as Function[])
+        );
+        return _mw;
+      },
+      [] as Function[]
+    );
     return [].concat.apply([], mw);
   }
 
-
   // Wrapper for staticGzip
-  public static(p: string, prefix?: string) {
+  public static(
+    p: string,
+    prefix?: string,
+    position: Origami.Server.Position = 'post-render'
+  ) {
     const r = new Route(prefix || '/');
-    // r.use(express.static(path));
+    if (position) r.position(position);
     r.use(staticGzip(p));
     this.useRouter(r);
   }
 
-
   // Registers all the middleware and serves the app
   private async _setup() {
     // Setup the store
-    if (this.store) { this.app.set('store', this.store); }
-
+    if (this.store) {
+      this.app.set('store', this.store);
+    }
 
     this.app.use(fileUpload());
-    this.app.use(helmet({
-      frameguard: {
-        action: 'allow-from',
-        domain: '*'
-      }
-    }));
+    this.app.use(
+      helmet({
+        frameguard: {
+          action: 'allow-from',
+          domain: '*'
+        }
+      })
+    );
     this.app.use(corser.create());
-
 
     await this._setupStatic();
 
@@ -314,58 +346,58 @@ export class Server {
     await this._setupMiddleware();
   }
 
-
   private async _setupMiddleware() {
     this.app.use(content as RequestHandler);
 
     this._position('init');
 
-    this.app.use(bodyParser.urlencoded({
-      extended: true
-    }));
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: true
+      })
+    );
     this.app.use(bodyParser.json());
-
 
     // PRE-STORE position
     this._position('pre-store');
     this._position('store');
     this._position('post-store');
 
-
     // PRE-RENDER position
     this._position('pre-render');
     this._position('render');
     this._position('post-render');
 
-
     // Wrap for friendly errors
     this.app.use(mwErrors);
-
 
     // PRE-SEND position
     this._position('pre-send');
     this.app.use(mwFormat());
   }
 
-
   private async _setupStatic() {
-    if (this._options.static === false) { return; }
+    if (this._options.static === false) {
+      return;
+    }
 
     if (this._options.static === undefined || this._options.static === true) {
       try {
-        (await statAsync(path.join(process.cwd(), DEFAULT_STATIC))).isDirectory();
+        (await statAsync(
+          path.join(process.cwd(), DEFAULT_STATIC)
+        )).isDirectory();
         this._options.static = DEFAULT_STATIC;
-      } catch { }
+      } catch {}
     }
     let s = this._options.static as string | string[];
 
-
     if (s) {
-      if (!(s instanceof Array)) { s = [s]; }
+      if (!(s instanceof Array)) {
+        s = [s];
+      }
       s.forEach((_s) => this.static(path.resolve(process.cwd(), _s)));
     }
   }
-
 
   // Run the middleware for the router position
   private _position(pos: Origami.Server.Position) {
